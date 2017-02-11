9acc7fecb21927e910bb55bd450047fb338db9d9
Christian Kotzbauer <christian.kotzbauer@gmail.com>
2017-02-11 16:46:31 +0100

    use subtree command

--- deploy-page.sh
+++ deploy-page.sh
@@ -3,21 +3,9 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "source" ]; then e
 
 set -o errexit
 
-rm -rf public
-
 # config
 git config --global user.email "christian.kotzbauer@gmail.com"
 git config --global user.name "Christian Kotzbauer"
 
-git clone https://${GITHUB_TOKEN}@github.com/code-chris/code-chris.github.io.git -b master public
-
-# build
-rm -r -f public/assets public/content public/posts
-rm public/feed.xml public/index.html public/robots.txt public/sitemap.xml
-cp -r build/out/**/* public
-
 # deploy
-cd public
-git add .
-git commit -m "Deploy to Github Pages"
-git push origin master
+git subtree push --prefix=build/out https://${GITHUB_TOKEN}@github.com/code-chris/code-chris.github.io.git master
